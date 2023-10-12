ce355bba90bd92722fbc43cf47142b59a79d581c
YeongChaeYun7 <xerochae@gmail.com>
2023-10-12 21:35:12 +0900

    디자인 패키지 비교

--- ch.2/design_package_test/lib/main.dart
+++ ch.2/design_package_test/lib/main.dart
@@ -1,5 +1,8 @@
+// 머터리얼 디자인과 쿠퍼티노 디자인 비교
+// 머터리얼 디자인은 구조화해서 사용 가능한 반면, 쿠퍼티노 디자인은 앱바만 구조화 가능, 바디와 플로팅 액션 버튼 속성이 없어 직접 만들어야 함
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart'; // 머터리얼 디자인 적용을 위해 피키지 import
+// import 'package:flutter/material.dart'; // 머터리얼 디자인 적용을 위해 피키지 import
+import 'package:flutter/cupertino.dart';// 쿠퍼티노 디자인 적용을 위해 피키지 import
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +13,18 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true, // 머터리얼 디자인 3 적용
-      ),
+  Widget build(BuildContext context) 
+    { // 쿠퍼티노 디자인 적용
+    return CupertinoApp(
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
+  // { // 머터리얼 디자인 적용
+  //   return MaterialApp(
+  //     theme: ThemeData(
+  //       useMaterial3: true, // 머터리얼 디자인 3 적용
+  //     ),
+  //     home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  //   );
   }
 }
 
@@ -41,30 +48,57 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+    return CupertinoPageScaffold(
+            navigationBar: const
+            CupertinoNavigationBar(
+              middle: Text('cupertino design app'),
+            ),
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('material design app'),
+    //   ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: CupertinoTheme.of(context)
+              .textTheme
+              .navLargeTitleTextStyle
             ),
-          ],
+            const SizedBox(height: 60.0,), 
+
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'You have pushed the button this many times:',
+      //       ),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headlineMedium,
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      Center(
+        child: CupertinoButton.filled(
+          onPressed: _incrementCounter, child: const Icon(CupertinoIcons.add), 
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
         ),
+            ]
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      )
     );
   }
 }
